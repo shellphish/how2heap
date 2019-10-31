10849f6b227be4689e728c3d604fe71a69ec4976
Moritz Eckert <mo@mightym0.de>
2019-10-31 19:09:28 +0100

    Make first_fit go into unsorted_bin

--- first_fit.c
+++ first_fit.c
@@ -10,12 +10,12 @@ int main()
 	fprintf(stderr, "This can be exploited in a use-after-free situation.\n");
 
 	fprintf(stderr, "Allocating 2 buffers. They can be large, don't have to be fastbin.\n");
-	char* a = malloc(512);
-	char* b = malloc(256);
+	char* a = malloc(0x512);
+	char* b = malloc(0x256);
 	char* c;
 
-	fprintf(stderr, "1st malloc(512): %p\n", a);
-	fprintf(stderr, "2nd malloc(256): %p\n", b);
+	fprintf(stderr, "1st malloc(0x512): %p\n", a);
+	fprintf(stderr, "2nd malloc(0x256): %p\n", b);
 	fprintf(stderr, "we could continue mallocing here...\n");
 	fprintf(stderr, "now let's put a string at a that we can read later \"this is A!\"\n");
 	strcpy(a, "this is A!");
@@ -24,11 +24,11 @@ int main()
 	fprintf(stderr, "Freeing the first one...\n");
 	free(a);
 
-	fprintf(stderr, "We don't need to free anything again. As long as we allocate something between 505 and 520, it will end up at %p\n", a);
+	fprintf(stderr, "We don't need to free anything again. As long as we allocate smaller than 0x512, it will end up at %p\n", a);
 
-	fprintf(stderr, "So, let's allocate 506 bytes\n");
+	fprintf(stderr, "So, let's allocate 0x500 bytes\n");
 	c = malloc(506);
-	fprintf(stderr, "3rd malloc(506): %p\n", c);
+	fprintf(stderr, "3rd malloc(0x500): %p\n", c);
 	fprintf(stderr, "And put a different string here, \"this is C!\"\n");
 	strcpy(c, "this is C!");
 	fprintf(stderr, "3rd allocation %p points to %s\n", c, c);
