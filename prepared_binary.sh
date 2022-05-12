5f3d72621c73f77ef9f8144db1d52437bcad7f2c
Aleksey Kislitsa <aleksey.kislitsa@gmail.com>
2022-05-12 18:07:56 +0300

    Added switch for simple execution (default variant), execution over gdb/radare2 (keys -g/r2) simple preparing binary without execution (-p) update list of libc ib glibc-all-in-one (-u) download libc if it's needed (-r)

--- prepared_binary.sh
+++ prepared_binary.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
 VERSION="./glibc_versions"
-DIR_TCACHE='tcache'
+#DIR_TCACHE='tcache'
+DIR_TCACHE=''
 DIR_HOST='x64'
 LIB_HOST='amd64'
 GLIBC_VERSION=''
 TARGET=''
 UPDATE=''
 RELOAD=''
+GDB=''
+RADARE2=''
+NOT_EXECUTION=''
 
 # Handle arguments
 function show_help {
-    echo "Usage: $0 <version> <target> [-h] [-disable-tcache] [-i686]"
+    #echo "Usage: $0 <version> <target> [-h] [-disable-tcahe] [-i686] [-u] [-r] [-g [-r2] [-p]"
+    echo "Usage: $0 <version> <target> [-h] [-i686] [-u] [-r] [-g [-r2] [-p]"
+    echo "-i686 - use x32 bits libc"
+    echo "-u - update libc list in glibc-sll-in-one"
+    echo "-r - download libc in glibc-all-in-one"
+    echo "-g - start target in GDB"
+    echo "-r2 - start target in radare2"
+    echo "-p - just set interpreter and rpath in target without execution"
 }
 
 if [[ $# < 2 ]]; then
@@ -28,9 +39,9 @@ while :; do
             show_help
             exit
             ;;
-        -disable-tcache)
-            DIR_TCACHE='notcache'
-            ;;
+#        -disable-tcache)
+#            DIR_TCACHE='notcache'
+#            ;;
         -i686)
             DIR_HOST='i686'
 	    LIB_HOST='i386'
@@ -41,6 +52,15 @@ while :; do
 	-r)
 	    RELOAD='X'
 	    ;;
+	-g)
+	    GDB='X'
+	    ;;
+	-r2)
+	    RADARE2='X'
+	    ;;
+	-p)
+	   NOT_EXECUTION='X'
+	   ;;
         '')
             break
             ;;
@@ -48,28 +68,35 @@ while :; do
     shift
 done
 
-OUTPUT_DIR="$VERSION/$GLIBC_VERSION/${DIR_HOST}_${DIR_TCACHE}/lib"
-libc=$(cat glibc-all-in-one/list | grep "$GLIBC_VERSION" | grep "$LIB_HOST" | tail -n 1)
+if [ -z $DIR_TCACHE ]; then
+    OUTPUT_DIR="$VERSION/$GLIBC_VERSION/${DIR_HOST}/lib"
+else
+    OUTPUT_DIR="$VERSION/$GLIBC_VERSION/${DIR_HOST}_${DIR_TCACHE}/lib"
+fi
+
 if [ ! -d "$OUTPUT_DIR" ];
 then
     mkdir -p $OUTPUT_DIR
 fi
-if [ ! -f "$OUTPUT_DIR/libc-$GLIBC_VERSION.so" ]; then
-    if [ "$UPDATE" == "X" ] || [ "$RELOAD" == "X" ];
-    then
-        cd glibc-all-in-one
-        if [ "$UPDATE" == "X" ] || [ ! -f ./list ]; then
-            ./update_list
-        fi
 
-        if [ "$RELOAD" == "X" ] || [ ! -d libs/$libc ]; then
-            ./update_list
-            rm -rf libs/$libc debs/$libc
-            ./download $libc
-        fi
-        cd -
-    fi
-    cp -r glibc-all-in-one/libs/$libc/* $OUTPUT_DIR
+if [ "$UPDATE" == "X" ] || [ ! -f glibc-all-in-one/list ]; then
+    cd glibc-all-in-one
+    ./update_list
+    cd -
+fi
+
+libc=$(cat glibc-all-in-one/list | grep "$GLIBC_VERSION" | grep "$LIB_HOST" | tail -n 1)
+
+if [ "$RELOAD" == "X" ] || [ ! -d glibc-all-in-one/libs/$libc ]; then
+    cd glibc-all-in-one
+    ./update_list
+    rm -rf libs/$libc debs/$libc
+    ./download $libc
+    cd -
+fi
+
+if [ ! -f "$OUTPUT_DIR/libc-$GLIBC_VERSION.so" ]; then
+    cp -r glibc-all-in-one/libs/$libc/* $OUTPUT_DIR/
 fi
 
 curr_interp=$(patchelf --print-interpreter "$TARGET")
@@ -78,6 +105,7 @@ target_interp="$OUTPUT_DIR/ld-$GLIBC_VERSION.so"
 if [[ $curr_interp != $target_interp ]];
 then
     patchelf --set-interpreter "$target_interp" "$TARGET"
+    echo "INERPERETER as $target_interp for $TARGET"
 fi
 
 curr_rpath=$(patchelf --print-rpath "$TARGET")
@@ -85,5 +113,15 @@ curr_rpath=$(patchelf --print-rpath "$TARGET")
 if [[ $curr_rpath != $OUTPUT_DIR ]];
 then
     patchelf --set-rpath "$OUTPUT_DIR" "$TARGET"
+    echo "RPATH as $OUTPUT_DIR"
 fi
-"$TARGET"
+
+if [ "$GDB" == 'X' ]; then
+    gdb $TARGET
+elif [ "$RADARE2" == 'X' ]; then
+    r2 -d $TARGET
+elif [ "$NOT_EXECUTION" == ''  ]; then
+    "$TARGET"
+else
+    echo "$TARGET It's ready for discovering"
+fi
