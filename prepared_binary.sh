e9bc09150116f012920bdb76b492c1cdb28def90
Aleksey Kislitsa <aleksey.kislitsa@gmail.com>
2022-05-13 16:00:39 +0300

    Was corrected problem with working on old system like Ubuntu Focal with glibc more new then system glibc

--- prepared_binary.sh
+++ prepared_binary.sh
@@ -12,17 +12,20 @@ RELOAD=''
 GDB=''
 RADARE2=''
 NOT_EXECUTION=''
+FORCE_TARGET_INTERPRETER=''
 
 # Handle arguments
 function show_help {
     #echo "Usage: $0 <version> <target> [-h] [-disable-tcahe] [-i686] [-u] [-r] [-g [-r2] [-p]"
-    echo "Usage: $0 <version> <target> [-h] [-i686] [-u] [-r] [-g [-r2] [-p]"
+    echo "Usage: $0 <version> <target> [-h] [-i686] [-u] [-r] [-g [-r2] [-p] [-si] [-ti]"
     echo "-i686 - use x32 bits libc"
     echo "-u - update libc list in glibc-sll-in-one"
     echo "-r - download libc in glibc-all-in-one"
     echo "-g - start target in GDB"
     echo "-r2 - start target in radare2"
     echo "-p - just set interpreter and rpath in target without execution"
+    #echo "-si - set system interpreter"
+    echo "-ti - force to set target interpreter"
 }
 
 if [[ $# < 2 ]]; then
@@ -31,7 +34,7 @@ if [[ $# < 2 ]]; then
 fi
 
 function update_glibc (){
-    if [ "$UPDATE" == "X" ] || [ ! -f glibc-all-in-one/list ]; then
+    if [ "$1" == "X" ] || [ ! -f glibc-all-in-one/list ]; then
         cd glibc-all-in-one
         ./update_list
         cd -
@@ -39,30 +42,32 @@ function update_glibc (){
 }
 
 function download_glibc (){
-    if [ "$RELOAD" == "X" ] || [ ! -d glibc-all-in-one/libs/$libc ]; then
+    if [ "$2" == "X" ] || [ ! -d glibc-all-in-one/libs/$libc ]; then
         cd glibc-all-in-one
         rm -rf libs/$1 debs/$1
         ./download $1
+        ./download_old $1
         cd -
     fi
 }
 
 function copy_glibc (){
     if [ ! -f "$OUTPUT_DIR/libc-$GLIBC_VERSION.so" ]; then
-        cp -rP glibc-all-in-one/libs/$1/* $OUTPUT_DIR
-        cp -rP glibc-all-in-one/libs/$1/.debug $OUTPUT_DIR
+        cp -r glibc-all-in-one/libs/$1/* $OUTPUT_DIR
+        cp -r glibc-all-in-one/libs/$1/.debug $OUTPUT_DIR
     fi
 }
 
 function set_interpreter (){
     
-    curr_interp=$(patchelf --print-interpreter "$TARGET")
-    target_interp="$OUTPUT_DIR/ld-$GLIBC_VERSION.so"
+    local curr_interp=$(patchelf --print-interpreter "$TARGET")
+    #local target_interp="$OUTPUT_DIR/ld-$GLIBC_VERSION.so"
+    #local target_interp=$(ls glibc_versions/2.34/x64/lib | grep ld)
     
-    if [[ $curr_interp != $target_interp ]];
+    if [[ $curr_interp != $1 ]];
     then
-        patchelf --set-interpreter "$target_interp" "$TARGET"
-        echo "INERPERETER as $target_interp for $TARGET"
+        patchelf --set-interpreter "$1" "$TARGET"
+        echo "INERPERETER as $1 for $TARGET"
     fi
 }
 
@@ -80,9 +85,10 @@ GLIBC_VERSION=$1
 GLIBC_MAJOR=$(echo $GLIBC_VERSION | cut -d'.' -f1)
 GLIBC_MINOR=$(echo $GLIBC_VERSION | cut -d'.' -f2)
 TARGET=$2
-SYSTEM_GLIBC=$(lsof -p $$ 2>/dev/null | grep libc- | awk ' { print $NF" --version"; } ' | sh | head -n 1 | cut -d' ' -f 10 | cut -d'.' -f 1-2)
-SYSTEM_GLIBC_MAJOR=$(echo $SYSTEM_GLIBC | cut -d'.' -f1)
-SYSTEM_GLIBC_MINOR=$(echo $SYSTEM_GLIBC | cut -d'.' -f2)
+SYSTEM_GLIBC_VERSION=$(lsof -p $$ 2>/dev/null | grep libc- | awk ' { print $NF" --version"; } ' | sh | head -n 1 | cut -d' ' -f 10 | cut -d'.' -f 1-2)
+SYSTEM_GLIBC_MAJOR=$(echo $SYSTEM_GLIBC_VERSION | cut -d'.' -f1)
+SYSTEM_GLIBC_MINOR=$(echo $SYSTEM_GLIBC_VERSION | cut -d'.' -f2)
+SYSTEM_GLIBC=$(lsof -p $$ 2>/dev/null | grep libc- | awk ' { print $NF" --version"; } ' | sh | head -n 1 | cut -d' ' -f 10)
 
 while :; do
     case $3 in
@@ -112,6 +118,9 @@ while :; do
         -p)
             NOT_EXECUTION='X'
         ;;
+        -ti)
+	    FORCE_TARGET_INTERPRETER='X'
+	;;
         '')
             break
         ;;
@@ -130,21 +139,30 @@ then
     mkdir -p $OUTPUT_DIR
 fi
 
-update_glibc
+update_glibc $UPDATE
 libc=$(cat glibc-all-in-one/list | grep "$GLIBC_VERSION" | grep "$LIB_HOST" | head -n 1)
-download_glibc $libc
+download_glibc $libc $RELOAD
 copy_glibc $libc
+if [ -z "$(ls -A $OUTPUT_DIR)" ]; then
+    echo "Couldn't download and extract glibc."
+    echo "Check you have installed zstd"
+    exit
+fi
+target_interpreter="$OUTPUT_DIR/$(ls $OUTPUT_DIR | grep ld)"
 
 if [[ "$GLIBC_MAJOR" < "$SYSTEM_GLIBC_MAJOR" ]];
 then
-    set_interpreter
+    set_interpreter $target_interpreter
     set_rpath
 else
     if [[ "$GLIBC_MINOR" < "$SYSTEM_GLIBC_MINOR" ]];
     then
-        set_interpreter
+        set_interpreter $target_interpreter
         set_rpath
     else
+	if [ ! -z $FORCE_TARGET_INTERPRETER ]; then
+            set_interpreter $target_interpreter
+	fi
         set_rpath
     fi
 fi
