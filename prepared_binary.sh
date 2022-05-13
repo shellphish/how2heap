ac05decff063edb983428bf932a5d9d9b838546a
Aleksey Kislitsa <aleksey.kislitsa@gmail.com>
2022-05-13 10:38:36 +0300

    Added checking system glibc version, to patch if system glibc newer than target glibc

--- prepared_binary.sh
+++ prepared_binary.sh
@@ -30,40 +30,90 @@ if [[ $# < 2 ]]; then
     exit 1
 fi
 
+function update_glibc (){
+    if [ "$UPDATE" == "X" ] || [ ! -f glibc-all-in-one/list ]; then
+        cd glibc-all-in-one
+        ./update_list
+        cd -
+    fi
+}
+
+function download_glibc (){
+    if [ "$RELOAD" == "X" ] || [ ! -d glibc-all-in-one/libs/$libc ]; then
+        cd glibc-all-in-one
+        rm -rf libs/$libc debs/$libc
+        ./download $libc
+        cd -
+    fi
+}
+
+function copy_glibc (){
+    if [ ! -f "$OUTPUT_DIR/libc-$GLIBC_VERSION.so" ]; then
+        cp -r glibc-all-in-one/libs/$libc/. $OUTPUT_DIR
+    fi
+}
+
+function set_interpreter (){
+    
+    curr_interp=$(patchelf --print-interpreter "$TARGET")
+    target_interp="$OUTPUT_DIR/ld-$GLIBC_VERSION.so"
+    
+    if [[ $curr_interp != $target_interp ]];
+    then
+        patchelf --set-interpreter "$target_interp" "$TARGET"
+        echo "INERPERETER as $target_interp for $TARGET"
+    fi
+}
+
+function set_rpath (){
+    curr_rpath=$(patchelf --print-rpath "$TARGET")
+    
+    if [[ $curr_rpath != $OUTPUT_DIR ]];
+    then
+        patchelf --set-rpath "$OUTPUT_DIR" "$TARGET"
+        echo "RPATH as $OUTPUT_DIR"
+    fi
+}
+
 GLIBC_VERSION=$1
+GLIBC_MAJOR=$(echo $GLIBC_VERSION | cut -d'.' -f1)
+GLIBC_MINOR=$(echo $GLIBC_VERSION | cut -d'.' -f2)
 TARGET=$2
+SYSTEM_GLIBC=$(lsof -p $$ 2>/dev/null | grep libc- | awk ' { print $NF" --version"; } ' | sh | head -n 1 | cut -d' ' -f 10 | cut -d'.' -f 1-2)
+SYSTEM_GLIBC_MAJOR=$(echo $SYSTEM_GLIBC | cut -d'.' -f1)
+SYSTEM_GLIBC_MINOR=$(echo $SYSTEM_GLIBC | cut -d'.' -f2)
 
 while :; do
     case $3 in
         -h|-\?|--help)
             show_help
             exit
-            ;;
-#        -disable-tcache)
-#            DIR_TCACHE='notcache'
-#            ;;
+        ;;
+        #        -disable-tcache)
+        #            DIR_TCACHE='notcache'
+        #            ;;
         -i686)
             DIR_HOST='i686'
-	    LIB_HOST='i386'
-            ;;
-	-u)
-	    UPDATE='X'
-	    ;;
-	-r)
-	    RELOAD='X'
-	    ;;
-	-g)
-	    GDB='X'
-	    ;;
-	-r2)
-	    RADARE2='X'
-	    ;;
-	-p)
-	   NOT_EXECUTION='X'
-	   ;;
+            LIB_HOST='i386'
+        ;;
+        -u)
+            UPDATE='X'
+        ;;
+        -r)
+            RELOAD='X'
+        ;;
+        -g)
+            GDB='X'
+        ;;
+        -r2)
+            RADARE2='X'
+        ;;
+        -p)
+            NOT_EXECUTION='X'
+        ;;
         '')
             break
-            ;;
+        ;;
     esac
     shift
 done
@@ -79,48 +129,34 @@ then
     mkdir -p $OUTPUT_DIR
 fi
 
-if [ "$UPDATE" == "X" ] || [ ! -f glibc-all-in-one/list ]; then
-    cd glibc-all-in-one
-    ./update_list
-    cd -
-fi
+update_glibc
+download_glibc
+copy_glibc
 
 libc=$(cat glibc-all-in-one/list | grep "$GLIBC_VERSION" | grep "$LIB_HOST" | head -n 1)
 
-if [ "$RELOAD" == "X" ] || [ ! -d glibc-all-in-one/libs/$libc ]; then
-    cd glibc-all-in-one
-    ./update_list
-    rm -rf libs/$libc debs/$libc
-    ./download $libc
-    cd -
-fi
-
-if [ ! -f "$OUTPUT_DIR/libc-$GLIBC_VERSION.so" ]; then
-    cp -r glibc-all-in-one/libs/$libc/. $OUTPUT_DIR
-fi
-
-curr_interp=$(patchelf --print-interpreter "$TARGET")
-target_interp="$OUTPUT_DIR/ld-$GLIBC_VERSION.so"
-
-if [[ $curr_interp != $target_interp ]];
+if [[ "$GLIBC_MAJOR" < "$SYSTEM_GLIBC_MAJOR" ]];
 then
-    patchelf --set-interpreter "$target_interp" "$TARGET"
-    echo "INERPERETER as $target_interp for $TARGET"
+    set_interpreter
+    set_rpath
+else
+    if [[ "$GLIBC_MINOR" < "$SYSTEM_GLIBC_MINOR" ]];
+    then
+        set_interpreter
+        set_rpath
+    else
+        set_rpath
+    fi
 fi
 
-curr_rpath=$(patchelf --print-rpath "$TARGET")
-
-if [[ $curr_rpath != $OUTPUT_DIR ]];
+if [ "$GDB" == 'X' ];
 then
-    patchelf --set-rpath "$OUTPUT_DIR" "$TARGET"
-    echo "RPATH as $OUTPUT_DIR"
-fi
-
-if [ "$GDB" == 'X' ]; then
     gdb $TARGET --symbols $OUTPUT_DIR --se $TARGET
-elif [ "$RADARE2" == 'X' ]; then
+elif [ "$RADARE2" == 'X' ];
+then
     r2 -d $TARGET
-elif [ "$NOT_EXECUTION" == ''  ]; then
+elif [ "$NOT_EXECUTION" == ''  ];
+then
     "$TARGET"
 else
     echo "$TARGET It's ready for discovering"
